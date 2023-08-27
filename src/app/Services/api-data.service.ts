26218bef28e6988d7daf6b81088c3fa7d9faa2c3
mohamedemad201900715 <medoem82950@gmail.com>
2023-08-27 10:54:54 +0200

    Update api

--- src/app/Services/api-data.service.ts
+++ src/app/Services/api-data.service.ts
@@ -10,7 +10,7 @@ export class ApiDataService {
   constructor(private http:HttpClient) { }
 
   public getProducts(){
-  return this.http.get<ExchangeRateResponse>('https://currencyconversion-production-38ba.up.railway.app/currency/compare?base=usd');
+  return this.http.get<ExchangeRateResponse>('https://currencyconversion-production-38ba.up.railway.app/currency/compare?base=USD');
 }
 getConvert(from: string, to: string, amount: number){
   return this.http.get<number>(`https://currencyconversion-production-38ba.up.railway.app/currency/convert?fromCurrency=${from}&toCurrency=${to}&amount=${amount}`);
